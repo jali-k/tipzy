dc0ebdd83092aea0db6202f9c82e5bfba51dcdbd
jali-k <e19210@eng.pdn.ac.lk>
2024-03-05 23:55:05 +0530

    Configure firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tipzy/firebase_options.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:tipzy/screens/notifications_screen.dart';
 import 'package:tipzy/screens/projects_screen.dart';
 import 'package:tipzy/widgets/project_card.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
